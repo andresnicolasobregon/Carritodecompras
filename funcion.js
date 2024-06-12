7c7b59118238766b0abf36ba2f5b0ad3855d3a26
andresobregon <andresnicolasobregon@gmail.com>
2024-06-12 08:18:01 -0300

    independencia del array

--- funcion.js
+++ funcion.js
@@ -1,8 +1,3 @@
-const productos= [{id:0, nombre:"Sansung Galaxy",imagen:"celular1.png", precio:"$100"},
-				{id:1, nombre:"Sansung A4",imagen:"celular2.png", precio:"$200"},
-				{id:2, nombre:"Sansung A5",imagen:"celular3.png", precio:"$300"},
-				{id:3, nombre:"Sansung A5",imagen:"celular3.png", precio:"$300"}]
-
 function cargar() {
 	let padre = document.getElementById("boxproductos")
 	
